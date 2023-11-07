56eaf81fd165b3456e11f6a8b8dbd407b5f38b56
oniwinska <oniwinska@student.wsb-nlu.edu.pl>
2023-11-07 18:31:25 +0100

    zad 1

---
+++ src/Konwersja.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Konwersja
+{
+    public static void main(String[] args) {
+
+
+        /*int wiek;
+
+        double waga;
+        char inicjaly;
+        boolean kobieta;
+*/
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Podaj wiek : ");
+            int wiek = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.print("Podaj wage : ");
+            double waga = scanner.nextDouble();
+            scanner.nextLine();
+
+
+            System.out.print("Podaj pierwsza litere twojego imienia : ");
+            char a  = scanner.next().charAt(0);
+            scanner.nextLine();
+
+            System.out.print("Czy jestes kobieta?(true/false): ");
+            boolean kobieta = scanner.nextBoolean();
+
+            scanner.nextLine();
+
+            scanner.close();
+
+            System.out.println("Twoj wiek to: " + wiek);
+            System.out.println("Twoja waga to: " + waga);
+            System.out.println("Pierwsza litera twojego imienia to: " + a);
+            System.out.print("Jesteś kobieta?: ");
+            if (kobieta == true) {
+                    System.out.println("Prawda");
+            } else if (kobieta == false) {
+                    System.out.println("Falsz");
+            }
+
+
+            int wagaToInt =  (int)waga;
+            double wiekToDouble = (double)wiek;
+            int aToInt = (int)a;
+            String kobietaToString = String.valueOf(kobieta);
+
+            System.out.print(" Przed konwersją: " + wiek);
+            System.out.println( " Po konwersji: " + wiekToDouble);
+
+            System.out.print(" Przed konwersją: " + waga);
+            System.out.println(" Po konwersji: " + wagaToInt);
+
+            System.out.print(" Przed konwersją: " + a);
+            System.out.println(" Po konwersji: " + aToInt);
+
+            System.out.print(" Przed konwersją: " + kobieta);
+            System.out.println(" Po konwersji: " + kobietaToString);
+
+
+
+
+
+
+    }
+
+}

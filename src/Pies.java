a42c6912043a80cb950070b961b0dcf9160ccec3
oniwinska <oniwinska@student.wsb-nlu.edu.pl>
2023-11-07 20:54:11 +0100

    zad 3 i 4

---
+++ src/Pies.java
@@ -0,0 +1,21 @@
+public class Pies
+{
+    private int wiekPsa;
+    private String rasa;
+    private String kolor;
+    private double wagaPsa;
+
+    public Pies(int wiekPsa, String rasa, String kolor, double wagaPsa)
+    {
+        this.wiekPsa = wiekPsa;
+        this.rasa = rasa;
+        this.kolor = kolor;
+        this.wagaPsa = wagaPsa;
+    }
+
+    public int getWiekPsa()
+    {
+        return wiekPsa;
+    }
+
+}
